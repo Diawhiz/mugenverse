7944011697c7946e39e640e0c48848c8ee1d333b
Ariyo Israel Gbemisola <igbemisola53@gmail.com>
2024-08-13 13:01:46 +0100

    navbar responsiveness done

--- mugenverse/urls.py
+++ mugenverse/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import serve, static
-# from django.urls import re_path
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,8 +27,8 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# if settings.DEBUG:
-#     urlpatterns += [
-#         re_path(r'^(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-#     ]
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
 
